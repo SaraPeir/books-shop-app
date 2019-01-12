316ac920749657c8546ae7a3445bf0a9e80073e1
SaraPeir <sara.peirce1987@gmail.com>
2019-01-12 23:15:25 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./js/App";
+
+ReactDOM.render(
+        <App />
+    , document.getElementById('root'));
+
+//import { BrowserRouter, HashRouter } from 'react-router-dom';
+
+/* ReactDOM.render(
+    <HashRouter>
+        <App />
+    </HashRouter>, document.getElementById('root')); */
